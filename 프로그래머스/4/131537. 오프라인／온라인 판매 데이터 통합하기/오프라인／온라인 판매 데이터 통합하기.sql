b4d34b32fd899ae203a38e4597bd4a8bbdad000b
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-03-07 17:55:48 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,10 +1,10 @@
-    SELECT DATE_Format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, -- '2022-05-24'형식으로 포맷
+SELECT DATE_Format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, -- '2022-05-24'형식으로 포맷
            PRODUCT_ID, -- 제품 ID
            USER_ID,  -- 회원 ID
            SALES_AMOUNT -- 판매량
         from ONLINE_SALE -- 온라인 고객
         where Month(SALES_DATE) = 3
-union -- 쿼리의 결과를 합치고, 중복된 ROW는 제거한다. (오프라인/온라인의 쿼리 결과를 합친다는 의미)
+union all -- 쿼리의 결과를 합치고, 중복된 ROW는 제거한다. (오프라인/온라인의 쿼리 결과를 합친다는 의미)
     SELECT DATE_Format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, -- '2022-05-24'형식으로 포맷
            PRODUCT_ID, 
            Null as USER_ID, -- 오프라인 고객은 USER_ID가 없으므로 NULL값으로 채우고 
