e8ccad308680479556388589b5ce38f10a06790e
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-03-12 22:02:21 +0900

    [level 1] Title: 최소직사각형, Time: 5.70 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    answer = 0
+    w = []
+    h = []
+    for i in sizes:
+        w.append(max(i[0],i[1]))
+        h.append(min(i[0],i[1]))
+    
+    return max(w) * max(h)
+
