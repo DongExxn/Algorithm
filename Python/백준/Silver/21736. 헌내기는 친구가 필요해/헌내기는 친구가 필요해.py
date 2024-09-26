8eef53ae9c14f1caf2b3a4c59d864a3643cd5985
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-09-26 16:19:53 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 632 ms, Memory: 108800 KB -BaekjoonHub

---
+++ Python/백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+# 재귀 깊이를 늘려서 RecursionError를 방지
+sys.setrecursionlimit(10**6)
+
+def dfs(x, y):
+    global cnt 
+    visited[x][y] = True
+    
+    if graph[x][y] == 'P':
+        cnt += 1
+        
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            if graph[nx][ny] != "X":
+                dfs(nx, ny)
+
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1 ,1]
+cnt = 0
+n, m = map(int,input().split())
+graph = list(input() for _ in range(n))
+visited = [[False] * m for _ in range(n)]
+
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]=="I":
+            dfs(i, j)
+if cnt == 0:
+    print("TT")
+else:
+    print(cnt)
