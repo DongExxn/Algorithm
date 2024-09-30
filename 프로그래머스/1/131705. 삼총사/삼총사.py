1ae841db3a640b9a5ae3414b8cee2050df795b5c
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-09-30 23:12:31 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,11 @@
+import itertools
+
+def solution(number):
+    answer = 0
+    
+    nCr = list(itertools.combinations(number, 3)) 
+    
+    for i in nCr:
+        if sum(i) == 0:
+            answer += 1
+    return answer
