3f26b4c064c4cd0df320509c9947ca9a748ace2f
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-10-01 00:51:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = s
+    
+    arr = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    
+    for str, num in arr.items():
+        s = s.replace(str, num)
+        
+    return int(s)
