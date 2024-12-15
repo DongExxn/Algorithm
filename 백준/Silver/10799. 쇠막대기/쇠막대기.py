6d56a5d8310926e4a73db2bf7b6df22bbbf69742
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2024-12-15 18:21:20 +0900

    [Silver II] Title: 쇠막대기, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,15 +1,19 @@
-ir= input()
-stack=[]
-cnt = 0
-for i in range(len(ir)):
-    if ir[i] == "(":
-        stack.append("(")
-    else :
-        if ir[i-1]=="(":
-            stack.pop()
-            cnt+=len(stack) # 첫 번째 경우인 현재의 쇠막대기들을 카운팅합니다.
-            
-        else :
-            stack.pop()
-            cnt+=1 # 이 부분은 두 번째 경우인 나머지 부분을 세는 것입니다.
-print(cnt)
+def count_pieces(s):
+    stack = []
+    pieces = 0
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append('(')  # 새로운 쇠막대기 시작
+        else:
+            stack.pop()  # 쇠막대기 끝
+            if s[i-1] == '(':  # 바로 앞이 '('이면 레이저
+                pieces += len(stack)  # 스택에 있는 쇠막대기만큼 잘린 조각이 추가됨
+            else:  # 일반적인 닫는 괄호는 하나의 쇠막대기를 종료
+                pieces += 1
+    return pieces
+
+# 입력 받기
+s = input().strip()
+
+# 결과 출력
+print(count_pieces(s))
