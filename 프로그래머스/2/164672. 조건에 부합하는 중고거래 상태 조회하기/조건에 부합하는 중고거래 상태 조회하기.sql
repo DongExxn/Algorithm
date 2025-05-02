4694baa645ba850babac7686d380982cd3fd208d
Dongeun Kim <98999882+DongExxn@users.noreply.github.com>
2025-05-02 18:15:42 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID,WRITER_ID,TITLE,PRICE,CASE 
+        WHEN STATUS = 'DONE' THEN '거래완료'
+        WHEN STATUS = 'SALE' THEN '판매중'
+        ELSE '예약중'
+    END STATUS
+from USED_GOODS_BOARD
+where CREATED_DATE = '2022-10-05'
+order by BOARD_ID desc
